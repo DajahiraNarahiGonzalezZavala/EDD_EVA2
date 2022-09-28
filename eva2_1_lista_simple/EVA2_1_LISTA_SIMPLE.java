0f062deffef667fa8d5ad7588187b9050d2e944a
DajahiraNarahiGonzalezZavala <101366240+DajahiraNarahiGonzalezZavala@users.noreply.github.com>
2022-09-28 09:52:58 -0600

    Add files via upload

---
+++ eva2_1_lista_simple/EVA2_1_LISTA_SIMPLE.java
@@ -0,0 +1,15 @@
+
+package eva2_1_lista_simple;
+
+/**
+ *
+ * @author DAJAHIRA NARAHI GONZALEZ ZAVALA 21550340
+ */
+public class EVA2_1_LISTA_SIMPLE {
+
+ 
+    public static void main(String[] args) {
+
+    }
+    
+}
